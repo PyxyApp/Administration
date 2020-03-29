1d586bbfb8416f0d37457f6ddb51a3e703501a03
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-03-29 23:19:19 +0200

    add icon and change username to photo for user logged

--- src/components/views/Header.js
+++ src/components/views/Header.js
@@ -1,8 +1,9 @@
 import * as firebase from "firebase";
 import React, { Component } from 'react';
-import {Nav, Navbar} from "react-bootstrap";
+import {Badge, Image, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {history} from "../../redux/store/defaultStore";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBell, faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
 require("firebase/auth");
 require("firebase/firestore");
 
@@ -21,19 +22,48 @@ class Header extends Component {
         return(
             <div>
                 {history.location.pathname !== '/login' ? (
-                    <Navbar bg="dark" variant="dark">
+                    <Navbar>
                         <Navbar.Brand href="#home">PYXY</Navbar.Brand>
                         {/* Remove display user during front dev */}
                         {/*<NavDropdown title={(user.displayName !== null) ? (user.displayName) : (user.email)} id="basic-nav-dropdown">*/}
-                        <NavDropdown title="Nicolas Notararigo" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/profile/settings">Settings</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
-                        </NavDropdown>
                         <Nav className="mr-auto">
                             <Nav.Link to="/">Dashboard</Nav.Link>
                             <Nav.Link to="/users">Users</Nav.Link>
                         </Nav>
+                        <NavDropdown title={(
+                            <span>
+                            <FontAwesomeIcon icon={faBell} />
+                            <Badge pill variant="danger" className={"Notifications"}>
+                            5
+                            </Badge>
+                            </span>
+                        )} id="basic-nav-dropdown">
+                            <NavDropdown.Item disabled>Notifications</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title={(
+                            <span>
+                                <FontAwesomeIcon icon={faEnvelopeOpenText} />
+                                <Badge pill variant="info" className={"Notifications"}>
+                                    4
+                                  </Badge>
+                            </span>
+                        )} id="basic-nav-dropdown">
+                            <NavDropdown.Item disabled>Messages</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title={(
+                            <Image roundedCircle
+                                   src="https://avatars0.githubusercontent.com/u/13368283?s=460&u=80a43e0502d07ae15fe7a83b546b4e63fb9fb837&v=4"
+                                   alt=""
+                                   height={"35px"} />
+                                )} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/profile/settings">Settings</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/logout">Logout</NavDropdown.Item>
+                        </NavDropdown>
                     </Navbar>
                 ) : "" }
             </div>
