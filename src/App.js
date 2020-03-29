0725dc46404011aa6f6cf07524e2d51701c91a47
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-03-30 01:48:23 +0200

    login -> hide header and menu component, and re-order component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Route } from 'react-router'
 import { ConnectedRouter } from 'connected-react-router';
-import Index from './components/containers/Index';
-import Login from './components/containers/Login';
-import Header from './components/containers/Header';
+import Index from './components/containers/Content/Index';
+import Login from './components/containers/Auth/Login';
+import Header from './components/containers/Menu/Header';
+import Menu from './components/containers/Menu/Menu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = ({history}) => {
-  return (
-      <ConnectedRouter history={history}>
-              <Route path="/" component={Header}/>
-              <Route path="/" component={Index}/>
-              <Route exact path="/login" component={Login}/>
-      </ConnectedRouter>
-  );
+    if (history.location.pathname === '/login') {
+        return (
+            <ConnectedRouter history={history}>
+                <Route exact path="/login" component={Login}/>
+            </ConnectedRouter>
+            )
+    } else {
+        return (
+            <ConnectedRouter history={history}>
+                <Route path="/" component={Header}/>
+                <Route exact path="/" component={Index}/>
+                <Route exact path="/login" component={Login}/>
+                <Route path="/" component={Menu}/>
+            </ConnectedRouter>
+            )
+    }
 };
 
 export default App;
