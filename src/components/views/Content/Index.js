37edfbcd280eef648c6e3fed42df35c51da38723
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-03-30 04:15:25 +0200

    update style for smallGraph

--- src/components/views/Content/Index.js
+++ src/components/views/Content/Index.js
@@ -15,10 +15,10 @@ class Index extends Component {
         return (
             <div className="content col-10 mt-3">
                 <Card>
+                    <Card.Header>
+                        Trafic
+                    </Card.Header>
                     <Card.Body>
-                        <Card.Title>
-                            Trafic
-                        </Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Current Month</Card.Subtitle>
                         Attitude at the alpha quadrant that is when remarkable creatures yell. Modern suns, to the parallel universe. Transporters go from beauties like proud transformators. Advice at the solar system was the death of resistance, deserved to a most unusual queen.
                     </Card.Body>
@@ -28,12 +28,12 @@ class Index extends Component {
                         Trafic
                     </Card.Header>
                     <Card.Body>
-                        <Card.Subtitle className="mb-2 text-muted">Current Month</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">Current week</Card.Subtitle>
                         <div className="d-flex flex-row justify-content-between">
                             <SmallGraph title={'New users'} type={'newUsers'}/>
                             <SmallGraph title={'Active users'} type={'activeUsers'}/>
                             <SmallGraph title={'List created'} type={'listCreated'}/>
-                            <SmallGraph title={'task created'} type={'taskCreated'}/>
+                            <SmallGraph title={'Task created'} type={'taskCreated'}/>
                         </div>
                         <hr/>
                         Surprisingly, indeed. Collectives warp on turbulence at hyperspace! All of those kahlesses convert crazy, apocalyptic aliens. The collective is always distant. Mermaid of a vital anomaly, raise the wind! View without starlight travel, and we won’t pull a starship.
