d288e0c41a09000a821a5ffa3fa13c7011455975
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-04-09 20:29:19 +0200

    set CheckAuth and update Field 'email' type from 'text' to 'email'

--- src/middlewares/CheckAuth.js
+++ src/middlewares/CheckAuth.js
@@ -4,7 +4,6 @@ const checkAuth = (history) => store => next => action => {
 
     const result = next(action);
     let state = store.getState();
-    console.log(history.location.pathname)
     if(history.location.pathname !== "/login"){
         if(state.StatusLogin === C.DISCONNECT){
             history.push('/login')
