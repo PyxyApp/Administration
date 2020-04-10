5093aad433e9699d71209fddb35d8558956c3404
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2020-04-10 06:30:13 +0200

    update readme

--- src/tools/routeAPI.js
+++ src/tools/routeAPI.js
@@ -3,6 +3,6 @@ console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === "prod"){
     routeAPI = "https://api.pyxy.space/";
 }else{
-    routeAPI = "https://us-central1-pyxy-f84e8.cloudfunctions.net/api//";
+    routeAPI = "https://us-central1-pyxy-f84e8.cloudfunctions.net/api/";
 }
 export default routeAPI
